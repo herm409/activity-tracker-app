73722271bb11417584a61b07896cc8750008b622
herm409 <herman@freedombychoice.com>
2025-10-05 07:36:13 -0500

    streak bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  // In Tailwind CSS v2, 'purge' is used. For v3, this would be 'content'.
+  // This tells Tailwind to scan these files for class names.
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}', // All component files in the src folder
+    './public/index.html',      // The main HTML file
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
